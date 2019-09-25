d54295d6ad4e2f8891bd7090ff29f52ee640632c
zhouweidi <zhouweidi@57787194-4ec7-4324-af3d-b87abe402d06>
2019-09-25 16:56:25 +0000

    * 完善vid抛出异常的测试用例
    git-svn-id: svn://zwd33.gicp.net/DVM3/trunk@20 57787194-4ec7-4324-af3d-b87abe402d06

--- Sources/DvmTests/OtherTests/BasicVid.cs
+++ Sources/DvmTests/OtherTests/BasicVid.cs
@@ -1,5 +1,6 @@
 ﻿using Dvm;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace DvmTests.OtherTests
 {
@@ -9,6 +10,10 @@ namespace DvmTests.OtherTests
 		[TestMethod]
 		public void BitsSchema()
 		{
+			Assert.ThrowsException<ArgumentException>(() => new Vid(0, 1, null));
+			Assert.ThrowsException<ArgumentException>(() => new Vid(1, 0, null));
+			Assert.ThrowsException<ArgumentException>(() => new Vid(1, Vid.MaxIndex + 1, null));
+
 			Assert.IsTrue(Vid.Empty.Data == 0);
 			Assert.IsTrue(Vid.Empty.Cid == 0);
 			Assert.IsTrue(Vid.Empty.Index == 0);
@@ -16,23 +21,24 @@ namespace DvmTests.OtherTests
 			Assert.IsTrue(Vid.Empty.IsEmpty);
 
 			var vid = new Vid(1, 2, "abc");
-			Assert.IsTrue(vid.Data == (((ulong)1 << (6 * 8)) | 2));
-			Assert.IsTrue(vid.Cid == 1);
-			Assert.IsTrue(vid.Index == 2);
+			Assert.AreEqual(vid.Data, (((ulong)1 << (6 * 8)) | 2));
+			Assert.AreEqual(vid.Cid, 1);
+			Assert.AreEqual(vid.Index, 2ul);
 			Assert.AreEqual(vid.Description, "abc");
 			Assert.IsFalse(vid.IsEmpty);
 		}
 
 		[TestMethod]
-		public void CompareAndOperators()
+		public void Compare()
 		{
 			var vid1 = new Vid(1, 2, "abc");
 			var vid2 = new Vid(1, 2, "abc def");
 			var vid3 = new Vid(11, 22, "aabbcc");
 
 			// Operators
-			Assert.IsTrue(Vid.Empty != vid1);
-			Assert.IsFalse(Vid.Empty == vid1);
+			Assert.AreNotEqual(vid1, Vid.Empty);
+			Assert.IsTrue(vid1 != Vid.Empty);
+			Assert.IsFalse(vid1 == Vid.Empty);
 
 			Assert.AreEqual(vid1, vid1);
 			Assert.IsTrue(vid1 == vid2);
@@ -40,19 +46,20 @@ namespace DvmTests.OtherTests
 
 			// IComparable<T>
 			Assert.IsTrue(vid1.CompareTo(vid1) == 0);
-			Assert.IsTrue(vid1.CompareTo(Vid.Empty) != 0);
-			Assert.IsTrue(Vid.Empty.CompareTo(vid1) != 0);
-			Assert.IsTrue(vid1.CompareTo(Vid.Empty) > 0 || Vid.Empty.CompareTo(vid1) > 0);
-
-			Assert.IsTrue(vid1.CompareTo(vid1) == 0);
 			Assert.IsTrue(vid1.CompareTo(vid2) == 0);
 			Assert.IsTrue(vid1.CompareTo(vid3) < 0);
 			Assert.IsTrue(vid3.CompareTo(vid1) > 0);
+
+			Assert.IsTrue(vid1.CompareTo(Vid.Empty) != 0);
+			Assert.IsTrue(Vid.Empty.CompareTo(vid1) != 0);
+			Assert.IsTrue(vid1.CompareTo(Vid.Empty) > 0);
 
 			// Equals
 			Assert.IsTrue(vid1.Equals((object)vid1));
 			Assert.IsTrue(vid1.Equals((object)vid2));
 			Assert.IsFalse(vid1.Equals((object)vid3));
+			Assert.IsFalse(vid1.Equals(null));
+			Assert.IsFalse(vid1.Equals("abc"));
 		}
 
 		[TestMethod]
