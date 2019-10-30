12cc8ef69ba2c41a3dbb2457d206fabd0e77dd91
zhouweidi <zhouweidi@57787194-4ec7-4324-af3d-b87abe402d06>
2019-10-30 16:42:41 +0000

    * CoroutineVipo支持Receive原语（任意消息、类型过滤、自定义过滤） * SimpleCoroutineVipo -> Voroutine * VoroutineExtension扩展方法
    git-svn-id: svn://zwd33.gicp.net/DVM3/trunk@41 57787194-4ec7-4324-af3d-b87abe402d06

--- Sources/Dvm/VipoAdapters.cs
+++ Sources/Dvm/VipoAdapters.cs
@@ -4,11 +4,29 @@ using System.Collections.Generic;
 
 namespace Dvm
 {
+	public class YieldInstruction
+	{
+	}
+
+	class ReceiveInstruction : YieldInstruction
+	{
+		public Func<Message, bool> Filter { get; private set; }
+		public Action<Message> Handler { get; private set; }
+
+		public ReceiveInstruction(Func<Message, bool> filter, Action<Message> handler)
+		{
+			Filter = filter;
+			Handler = handler;
+		}
+	}
+
 	public abstract class CoroutineVipo : Vipo
 	{
 		public TickTask TickTask { get; private set; }
 
 		IEnumerator m_enumerator;
+		bool m_finished;
+		ReceiveInstruction m_receive;
 
 		protected CoroutineVipo(Scheduler scheduler, string name)
 			: base(scheduler, name)
@@ -17,6 +35,9 @@ namespace Dvm
 
 		protected override void OnTick(TickTask tickTask)
 		{
+			if (m_finished)
+				return;
+
 			TickTask = tickTask;
 
 			try
@@ -24,11 +45,56 @@ namespace Dvm
 				if (m_enumerator == null)
 					m_enumerator = Coroutine();
 
-				if (!m_enumerator.MoveNext())
+				for (int messageIndex = -1; ;)
 				{
-					Destroy();
+					if (m_receive != null)
+					{
+						if (tickTask.Messages.Count == 0)
+							return;
 
-					DisposableObject.TryDispose(ref m_enumerator);
+						++messageIndex;
+
+						if (m_receive.Filter != null)
+						{
+							while (!m_receive.Filter(InMessages[messageIndex]))
+							{
+								if (++messageIndex >= InMessages.Count)
+									return;
+							}
+						}
+
+						m_receive.Handler(InMessages[messageIndex]);
+						m_receive = null;
+					}
+
+					if (m_enumerator.MoveNext())
+					{
+						if (!(m_enumerator.Current is YieldInstruction))
+							throw new VipoFaultException(Vid, "Yield return a non-YieldInstruction"); // 使用特定异常类表示这种情况
+
+						switch (m_enumerator.Current)
+						{
+							case ReceiveInstruction ri:
+								m_receive = ri;
+								break;
+
+							case null:
+								throw new VipoFaultException(Vid, "The yield instruction is null");
+
+							default:
+								throw new VipoFaultException(Vid, "Unsupported yield instruction");
+						}
+					}
+					else
+					{
+						Destroy();
+
+						DisposableObject.TryDispose(ref m_enumerator);
+
+						m_finished = true;
+
+						return;
+					}
 				}
 			}
 			finally
@@ -39,40 +105,79 @@ namespace Dvm
 
 		protected abstract IEnumerator Coroutine();
 
+		public YieldInstruction Receive(Action<Message> handler)
+		{
+			return new ReceiveInstruction(null, handler);
+		}
+
+		public YieldInstruction Receive(Func<Message, bool> filter, Action<Message> handler)
+		{
+			return new ReceiveInstruction(filter, handler);
+		}
+
+		public YieldInstruction Receive<TMessage>(Action<TMessage> handler)
+			where TMessage : Message
+		{
+			return new ReceiveInstruction(
+				message => message is TMessage,
+				message => handler((TMessage)message));
+		}
+
+		public YieldInstruction Receive<TMessage>(Func<TMessage, bool> filter, Action<TMessage> handler)
+			where TMessage : Message
+		{
+			return new ReceiveInstruction(
+				message => message is TMessage && filter((TMessage)message),
+				message => handler((TMessage)message));
+		}
+
 		public IReadOnlyList<Message> InMessages
 		{
 			get { return TickTask.Messages; }
 		}
 	}
 
-	public class SimpleCoroutineVipo : CoroutineVipo
+	public delegate IEnumerator VoroutineProcedure(Voroutine v);
+
+	public sealed class Voroutine : CoroutineVipo
 	{
-		CoroutineFunction m_coroutine;
-		Action<Exception> m_onError;
+		VoroutineProcedure m_procedure;
+		Action<Exception> m_handleError;
 
-		public delegate IEnumerator CoroutineFunction(SimpleCoroutineVipo vipo);
-
-		public SimpleCoroutineVipo(CoroutineFunction coroutine, Action<Exception> onError, Scheduler scheduler, string name)
+		public Voroutine(VoroutineProcedure procedure, Action<Exception> handleError, Scheduler scheduler, string name)
 			: base(scheduler, name)
 		{
-			if (coroutine == null)
-				throw new ArgumentNullException(nameof(coroutine));
+			if (procedure == null)
+				throw new ArgumentNullException(nameof(procedure));
 
-			m_coroutine = coroutine;
-			m_onError = onError;
-		}
-
-		protected override void OnError(Exception e)
-		{
-			if (m_onError != null)
-				m_onError(e);
-			else
-				base.OnError(e);
+			m_procedure = procedure;
+			m_handleError = handleError;
 		}
 
 		protected override IEnumerator Coroutine()
 		{
-			return m_coroutine(this);
+			return m_procedure(this);
+		}
+
+		protected override void OnError(Exception e)
+		{
+			if (m_handleError != null)
+				m_handleError(e);
+			else
+				base.OnError(e);
+		}
+	}
+
+	public static class VoroutineExtension
+	{
+		public static Voroutine CreateVoroutine(this Scheduler scheduler, VoroutineProcedure procedure, string name = null)
+		{
+			return new Voroutine(procedure, null, scheduler, name);
+		}
+
+		public static Voroutine CreateVoroutine(this Scheduler scheduler, VoroutineProcedure procedure, Action<Exception> handleError, string name = null)
+		{
+			return new Voroutine(procedure, handleError, scheduler, name);
 		}
 	}
 }
---
+++ Sources/DvmTests/VipoTests/Coroutines.cs
@@ -0,0 +1,166 @@
+﻿using Dvm;
+using DvmTests.SchedulerTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+namespace DvmTests.VipoTests
+{
+	[TestClass]
+	public class Coroutines : TestSchedulerBase
+	{
+		class MyMessage : Message
+		{
+			public int Value { get; private set; }
+
+			public MyMessage(Vid from, Vid to, int value)
+				: base(from, to)
+			{
+				Value = value;
+			}
+		}
+
+		public IEnumerator SenderVoroutine(Voroutine v)
+		{
+			Console.WriteLine($"SenderVoroutine '{v.Name}' START");
+
+			v.SendMessage(new Message(v.Vid, s_vidReceiver1));
+			v.SendMessage(new MyMessage(v.Vid, s_vidReceiver1, 0));
+			v.SendMessage(new MyMessage(v.Vid, s_vidReceiver1, 1));
+			v.SendMessage(new MyMessage(v.Vid, s_vidReceiver1, 2));
+
+			v.SendMessage(new Message(v.Vid, s_vidReceiver2));
+			v.SendMessage(new MyMessage(v.Vid, s_vidReceiver2, 0));
+			v.SendMessage(new MyMessage(v.Vid, s_vidReceiver2, 1));
+			v.SendMessage(new MyMessage(v.Vid, s_vidReceiver2, 2));
+
+			Console.WriteLine($"SenderVoroutine '{v.Name}' END");
+
+			yield break;
+		}
+
+		class ReceiverVipo : CoroutineVipo
+		{
+			public ReceiverVipo(Scheduler scheduler, string name)
+				: base(scheduler, name)
+			{
+			}
+
+			protected override IEnumerator Coroutine()
+			{
+				Console.WriteLine($"ReceiverVipo '{Name}' START");
+
+				yield return Receive(
+					message =>
+					{
+						Console.WriteLine($"ReceiverVipo '{Name}' receives message '{message.GetType().Name}'");
+					});
+
+				yield return Receive(
+					message => message is MyMessage,
+					message =>
+					{
+						Console.WriteLine($"ReceiverVipo '{Name}' receives message '{message.GetType().Name}' 0");
+					});
+
+				yield return Receive<MyMessage>(
+					message =>
+					{
+						Console.WriteLine($"ReceiverVipo '{Name}' receives message '{message.GetType().Name}' 1");
+					});
+
+				yield return Receive<MyMessage>(
+					message => message.Value == 2,
+					message =>
+					{
+						Console.WriteLine($"ReceiverVipo '{Name}' receives message '{message.GetType().Name}' 2");
+					});
+
+				Console.WriteLine($"ReceiverVipo '{Name}' END");
+			}
+		}
+
+		public IEnumerator ReceiverVoroutine(Voroutine v)
+		{
+			Console.WriteLine($"ReceiverVoroutine '{v.Name}' START");
+
+			yield return v.Receive(
+				message =>
+				{
+					Console.WriteLine($"ReceiverVoroutine '{v.Name}' receives message '{message.GetType().Name}'");
+				});
+
+			yield return v.Receive(
+				message => message is MyMessage,
+				message =>
+				{
+					Console.WriteLine($"ReceiverVoroutine '{v.Name}' receives message '{message.GetType().Name}' 0");
+				});
+
+			yield return v.Receive<MyMessage>(
+				message =>
+				{
+					Console.WriteLine($"ReceiverVoroutine '{v.Name}' receives message '{message.GetType().Name}' 1");
+				});
+
+			yield return v.Receive<MyMessage>(
+				message => message.Value == 2,
+				message =>
+				{
+					Console.WriteLine($"ReceiverVoroutine '{v.Name}' receives message '{message.GetType().Name}' 2");
+				});
+
+			Console.WriteLine($"ReceiverVoroutine '{v.Name}' END");
+		}
+
+		static Vid s_vidReceiver1;
+		static Vid s_vidReceiver2;
+
+		[TestMethod]
+		public void VipoExceptions()
+		{
+			HookConsoleOutput();
+
+			var receiver1 = new ReceiverVipo(TheScheduler, "Receiver1");
+			s_vidReceiver1 = receiver1.Vid;
+			receiver1.Start();
+
+			var receiver2 = TheScheduler.CreateVoroutine(ReceiverVoroutine, "Receiver2");
+			s_vidReceiver2 = receiver2.Vid;
+			receiver2.Start();
+
+			var sender = TheScheduler.CreateVoroutine(SenderVoroutine, "Sender");
+			sender.Start();
+
+			Sleep();
+
+			//receiver1.Destroy();
+			//receiver2.Destroy();
+			//sender.Destroy();
+
+			Assert.AreEqual(receiver1.Stage, VipoStage.Destroyed);
+			Assert.AreEqual(receiver2.Stage, VipoStage.Destroyed);
+			Assert.AreEqual(sender.Stage, VipoStage.Destroyed);
+
+			var consoleOutput = GetConsoleOutput();
+			{
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVipo 'Receiver1' START"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVipo 'Receiver1' receives message 'Message'"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVipo 'Receiver1' receives message 'MyMessage' 0"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVipo 'Receiver1' receives message 'MyMessage' 1"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVipo 'Receiver1' receives message 'MyMessage' 2"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVipo 'Receiver1' END"));
+
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVoroutine 'Receiver2' START"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVoroutine 'Receiver2' receives message 'Message'"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVoroutine 'Receiver2' receives message 'MyMessage' 0"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVoroutine 'Receiver2' receives message 'MyMessage' 1"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVoroutine 'Receiver2' receives message 'MyMessage' 2"));
+				Assert.IsTrue(consoleOutput.Contains("ReceiverVoroutine 'Receiver2' END"));
+
+				Assert.IsTrue(consoleOutput.Contains("SenderVoroutine 'Sender' START"));
+				Assert.IsTrue(consoleOutput.Contains("SenderVoroutine 'Sender' END"));
+			}
+		}
+	}
+}
