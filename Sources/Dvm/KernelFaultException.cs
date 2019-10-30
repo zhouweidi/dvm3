1401ee59e71b294556c713c9dcc4c087b4c006c9
zhouweidi <zhouweidi@57787194-4ec7-4324-af3d-b87abe402d06>
2019-10-30 13:04:29 +0000

    * 正式的class KernelFaultException
    git-svn-id: svn://zwd33.gicp.net/DVM3/trunk@37 57787194-4ec7-4324-af3d-b87abe402d06

---
+++ Sources/Dvm/KernelFaultException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dvm
+{
+	// All errors that blocks scheduler execution
+	public class KernelFaultException : Exception
+	{
+		public KernelFaultException()
+		{
+		}
+
+		public KernelFaultException(string message)
+			: base(message)
+		{
+		}
+	}
+}
