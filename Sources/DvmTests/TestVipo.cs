9b0089f3d03c472ec96adf1dfe0ff2a536f59e28
Gioveidi <zhouweidi@outlook.com>
2022-03-21 00:08:03 +0800

    Minor changes to unit test classes

--- Sources/DvmTests/TestVipo.cs
+++ Sources/DvmTests/TestVipo.cs
@@ -6,7 +6,7 @@ namespace DvmTests
 	{
 		readonly VmTestBase m_test;
 
-		public TestVipo(VmTestBase test, string symbol)
+		protected TestVipo(VmTestBase test, string symbol)
 			: base(test.VM, symbol)
 		{
 			m_test = test;
