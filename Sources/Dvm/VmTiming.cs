74a98de2223f8ce5cf974f2c8b1d1f033b8c0748
Gioveidi <zhouweidi@outlook.com>
2022-04-06 00:03:22 +0800

    VmTiming maintains a sorted request list

--- Sources/Dvm/VmTiming.cs
+++ Sources/Dvm/VmTiming.cs
@@ -6,11 +6,12 @@ using System.Linq;
 namespace Dvm
 {
 	// Vipos having timers set are weak referenced by a VmTiming object.
-	// They can be well-handled even they are disposed.
+	// They can be well-handled after they are disposed.
 	class VmTiming : VmThread
 	{
 		readonly BlockingCollection<Request> m_requestQueue = new BlockingCollection<Request>();
-		readonly Dictionary<Vid, Request> m_requests = new Dictionary<Vid, Request>();
+		readonly Dictionary<Vid, long> m_requests = new Dictionary<Vid, long>();
+		readonly SortedDictionary<long, LinkedList<Vid>> m_sortedRequests = new SortedDictionary<long, LinkedList<Vid>>();
 
 		internal static long Now => Environment.TickCount64;
 
@@ -79,12 +80,10 @@ namespace Dvm
 				}
 				else
 				{
-					var sortedRequests = SortRequests();
-
-					var timeoutMilliseconds = sortedRequests[0].DueTime - Now;
+					var timeoutMilliseconds = m_sortedRequests.First().Key - Now;
 					if (timeoutMilliseconds <= 0)
 					{
-						NotifyVipos(sortedRequests);
+						NotifyVipos();
 						continue;
 					}
 
@@ -101,7 +100,7 @@ namespace Dvm
 						while (m_requestQueue.TryTake(out request));
 					}
 					else
-						NotifyVipos(sortedRequests);
+						NotifyVipos();
 				}
 			}
 		}
@@ -109,35 +108,74 @@ namespace Dvm
 		void ProcessRequest(Request request)
 		{
 			if (request.DueTime == 0)
-				m_requests.Remove(request.Vid);
-			else
-				m_requests[request.Vid] = request;
-		}
-
-		Request[] SortRequests()
-		{
-			return (from r in m_requests.Values
-					orderby r.DueTime
-					select r).ToArray();
-		}
-
-		void NotifyVipos(IEnumerable<Request> sortedRequests)
-		{
-			var now = Now;
-
-			foreach (var request in sortedRequests)
 			{
-				if (request.DueTime > now)
+				// Remove
+				if (m_requests.TryGetValue(request.Vid, out long dueTime))
+				{
+					m_requests.Remove(request.Vid);
+
+					RemoveFromSorted(dueTime, request.Vid);
+				}
+			}
+			else
+			{
+				// Remove existed node
+				if (m_requests.TryGetValue(request.Vid, out long dueTime))
+				{
+					if (dueTime == request.DueTime)
+						return;
+
+					RemoveFromSorted(dueTime, request.Vid);
+				}
+
+				// Add a new node
+				if (!m_sortedRequests.TryGetValue(request.DueTime, out LinkedList<Vid> vidList))
+				{
+					vidList = new LinkedList<Vid>();
+					m_sortedRequests[request.DueTime] = vidList;
+				}
+
+				vidList.AddLast(request.Vid);
+
+				m_requests[request.Vid] = request.DueTime;
+			}
+		}
+
+		void RemoveFromSorted(long dueTime, Vid vid)
+		{
+			var vidList = m_sortedRequests[dueTime];
+
+			if (vidList.Remove(vid) && vidList.Count == 0)
+				m_sortedRequests.Remove(dueTime);
+		}
+
+		void NotifyVipos()
+		{
+			var removeList = new List<long>();
+
+			var now = Now;
+			foreach (var item in m_sortedRequests)
+			{
+				if (item.Key > now)
 					break;
 
 				// Notify
-				var vipo = request.Vid.ResolveVipo();
-				if (vipo != null && !vipo.Disposed)
-					vipo.InputMessage(SystemScheduleMessage.Timer.CreateVipoMessage());
+				foreach (var vid in item.Value)
+				{
+					var vipo = vid.ResolveVipo();
+					if (vipo != null && !vipo.Disposed)
+						vipo.InputMessage(SystemScheduleMessage.Timer.CreateVipoMessage());
 
-				// Remove
-				m_requests.Remove(request.Vid);
+					// Remove
+					m_requests.Remove(vid);
+				}
+
+				removeList.Add(item.Key);
 			}
+
+			// Remove
+			foreach (var item in removeList)
+				m_sortedRequests.Remove(item);
 		}
 
 		#endregion
