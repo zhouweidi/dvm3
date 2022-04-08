14658d73c316fe7f09b526e867f7abfac7674516
Gioveidi <zhouweidi@outlook.com>
2022-04-08 23:42:01 +0800

    AsyncVipo initial implementation

---
+++ Sources/Dvm/AsyncVipo.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Dvm
+{
+	public abstract class AsyncVipo : Vipo
+	{
+		Task m_task;
+		readonly LinkedList<AsyncOperation> m_operations = new LinkedList<AsyncOperation>();
+
+		#region Initialization
+
+		public AsyncVipo(VirtualMachine vm, string symbol)
+			: base(vm, symbol)
+		{
+		}
+
+		protected override void OnDispose()
+		{
+			if (m_task != null)
+				m_task.Dispose();
+
+			base.OnDispose();
+		}
+
+		#endregion
+
+		#region Run
+
+		protected override void Run(IVipoMessageStream messageStream)
+		{
+			if (m_task == null)
+				m_task = RunAsync();
+
+			while (m_task.Exception == null && m_operations.Count > 0 && messageStream.GetNext(out VipoMessage vipoMessage))
+			{
+				for (var node = m_operations.First; node != null;)
+				{
+					var op = node.Value;
+					if (op.TestCompleted(ref vipoMessage))
+					{
+						var remove = node;
+						node = node.Next;
+
+						m_operations.Remove(remove);
+
+						op.Continue();
+					}
+					else
+						node = node.Next;
+				}
+			}
+
+			if (m_task.Exception != null)
+				throw new Exception("RunAsync exception", m_task.Exception);
+		}
+
+		protected abstract Task RunAsync();
+
+		#endregion
+
+		#region AsyncOperation
+
+		public abstract class AsyncOperation : INotifyCompletion
+		{
+			protected readonly AsyncVipo m_vipo;
+			Action m_continuation;
+
+			protected AsyncOperation(AsyncVipo vipo)
+			{
+				m_vipo = vipo;
+			}
+
+			internal protected abstract bool TestCompleted(ref VipoMessage vipoMessage);
+
+			internal void Continue()
+			{
+				m_continuation.Invoke();
+			}
+
+			#region INotifyCompletion
+
+			void INotifyCompletion.OnCompleted(Action continuation)
+			{
+				// Start the task and call back to 'continuation' when completed
+				m_vipo.m_operations.AddLast(this);
+
+				m_continuation = continuation;
+			}
+
+			#endregion
+		}
+
+		#endregion
+
+		#region ReceiveOperation
+
+		public class ReceiveOperation : AsyncOperation
+		{
+			readonly int m_timerId;
+			readonly Func<VipoMessage, bool> m_condition;
+			VipoMessage m_vipoMessage;
+			bool m_completed;
+
+			internal ReceiveOperation(AsyncVipo vipo, int timeoutMilliseconds, Func<VipoMessage, bool> condition)
+				: base(vipo)
+			{
+				if (timeoutMilliseconds > 0)
+					m_timerId = vipo.SetTimer(timeoutMilliseconds);
+
+				m_condition = condition;
+			}
+
+			#region Await methods
+
+			public bool IsCompleted => m_completed;
+
+			public VipoMessage GetResult() => m_vipoMessage;
+
+			#endregion
+
+			internal protected override bool TestCompleted(ref VipoMessage vipoMessage)
+			{
+				if (m_timerId != 0 &&
+					vipoMessage.Message is UserTimerMessage timer &&
+					timer.TimerId == m_timerId)
+				{
+					m_completed = true;
+					return true;
+				}
+
+				if (m_condition == null || m_condition(vipoMessage))
+				{
+					m_vipoMessage = vipoMessage;
+					m_completed = true;
+
+					if (m_timerId != 0)
+						m_vipo.ResetTimer(m_timerId);
+
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		#endregion
+
+		#region SleepOperation
+
+		public class SleepOperation : AsyncOperation
+		{
+			readonly int m_timerId;
+			bool m_completed;
+
+			internal SleepOperation(AsyncVipo vipo, int timeoutMilliseconds)
+				: base(vipo)
+			{
+				if (timeoutMilliseconds <= 0)
+					throw new ArgumentException("<= 0", nameof(timeoutMilliseconds));
+
+				m_timerId = vipo.SetTimer(timeoutMilliseconds);
+			}
+
+			#region Await methods
+
+			public bool IsCompleted => m_completed;
+
+			public void GetResult() { }
+
+			#endregion
+
+			internal protected override bool TestCompleted(ref VipoMessage vipoMessage)
+			{
+				if (vipoMessage.Message is UserTimerMessage timer && timer.TimerId == m_timerId)
+				{
+					m_completed = true;
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		#endregion
+
+		#region Primitives
+
+		protected ReceiveOperation Receive(int timeoutMilliseconds = 0)
+		{
+			return new ReceiveOperation(this, timeoutMilliseconds, null);
+		}
+
+		protected ReceiveOperation Receive<TMessage>(int timeoutMilliseconds = 0)
+			where TMessage : Message
+		{
+			return new ReceiveOperation(this, timeoutMilliseconds, vm => vm.Message is TMessage);
+		}
+
+		protected ReceiveOperation Receive(Func<VipoMessage, bool> condition, int timeoutMilliseconds = 0)
+		{
+			return new ReceiveOperation(this, timeoutMilliseconds, condition);
+		}
+
+		protected ReceiveOperation ReceiveFrom(Vid from, int timeoutMilliseconds = 0)
+		{
+			return new ReceiveOperation(this, timeoutMilliseconds, vm => vm.From == from);
+		}
+
+		protected ReceiveOperation ReceiveFrom<TMessage>(Vid from, int timeoutMilliseconds = 0)
+			where TMessage : Message
+		{
+			return new ReceiveOperation(this, timeoutMilliseconds, vm => vm.From == from && vm.Message is TMessage);
+		}
+
+		protected SleepOperation Sleep(int timeoutMilliseconds)
+		{
+			return new SleepOperation(this, timeoutMilliseconds);
+		}
+
+		#endregion
+	}
+
+	public static class AsyncOperationAwaiters
+	{
+		public static AsyncVipo.ReceiveOperation GetAwaiter(this AsyncVipo.ReceiveOperation o) => o;
+		public static AsyncVipo.SleepOperation GetAwaiter(this AsyncVipo.SleepOperation o) => o;
+	}
+}
---
+++ Sources/DvmTests/VipoTests/Coroutines.cs
@@ -0,0 +1,148 @@
+﻿using Dvm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace DvmTests.VipoTests
+{
+	[TestClass]
+	public class Coroutines : VmTestBase
+	{
+		[TestMethod]
+		public void Basic()
+		{
+			var a = new MyAsyncVipo(this, "a");
+			var b = new MyVipo(this, "b");
+			Assert.AreEqual(VM.ViposCount, 2);
+
+			b.Schedule(a.Vid);
+
+			Sleep();
+
+			{
+				var output = GetCustomOutput();
+				Assert.IsTrue(output.Length == 3);
+				Assert.IsTrue(output[0] == "0 0");
+				Assert.IsTrue(output[1] == "1 1");
+				Assert.IsTrue(output[2] == "2 2");
+			}
+		}
+
+		class MyAsyncVipo : AsyncVipo
+		{
+			readonly VmTestBase m_test;
+
+			#region Test supports
+
+			public MyAsyncVipo(VmTestBase test, string symbol)
+				: base(test.VM, symbol)
+			{
+				m_test = test;
+			}
+
+			protected override void OnError(Exception e)
+			{
+				Assert.Fail(e.ToString());
+			}
+
+			protected void Print(string content = "")
+			{
+				m_test.Print(content);
+			}
+
+			#endregion
+
+			protected override async Task RunAsync()
+			{
+				Vid from;
+				{
+					var vipoMessage = await Receive();
+					if (vipoMessage.Message is TestMessage message)
+					{
+						Print($"0 {message.Value}");
+
+						from = vipoMessage.From;
+					}
+					else
+					{
+						Assert.Fail($"Unexpected message {vipoMessage}");
+						from = Vid.Empty;
+					}
+				}
+
+				{
+					var vipoMessage = await ReceiveFrom(from, 200);
+					if (vipoMessage.Message is TestMessage message)
+						Print($"1 {message.Value}");
+					else
+						Assert.Fail($"Unexpected message {vipoMessage}");
+				}
+
+				{
+					var now = VmTiming.Now;
+
+					await Sleep(50);
+
+					var duration = VmTiming.Now - now;
+					Assert.IsTrue(duration >= 50);
+				}
+
+				{
+					var vipoMessage = await ReceiveFrom<TestMessage>(from);
+					if (vipoMessage.Message is TestMessage message)
+						Print($"2 {message.Value}");
+					else
+						Assert.Fail($"Unexpected message {vipoMessage}");
+				}
+			}
+		}
+
+		class MyVipo : TestVipo
+		{
+			Vid m_targetVid;
+			int m_timerId;
+			int m_value;
+
+			public MyVipo(VmTestBase test, string symbol)
+				: base(test, symbol)
+			{
+			}
+
+			protected override void Run(IVipoMessageStream messageStream)
+			{
+				while (messageStream.GetNext(out VipoMessage vipoMessage))
+				{
+					switch (vipoMessage.Message)
+					{
+						case UserScheduleMessage schedule:
+							m_targetVid = (Vid)schedule.Context;
+							m_timerId = SetRepeatedTimer(100);
+							break;
+
+						case UserTimerMessage _:
+							Send(m_targetVid, new TestMessage()
+							{
+								Value = m_value
+							});
+
+							if (++m_value == 3)
+							{
+								ResetTimer(m_timerId);
+								m_timerId = -1;
+							}
+							break;
+
+						default:
+							Assert.Fail("Unknown message");
+							break;
+					}
+				}
+			}
+		}
+
+		class TestMessage : Message
+		{
+			public int Value;
+		}
+	}
+}
